5de03f1f88a1cfeb98f5049d9afe9e593921da4e
manjotkaur3112 <manjotkaur311205@gmail.com>
2025-11-03 21:58:23 +0530

    Update the file for Managing the access

--- admin/view_logs.php
+++ admin/view_logs.php
@@ -1,7 +1,6 @@
 <?php
 include_once '../config/db.php';
 include_once '../includes/log_action.php';
-session_start();
 
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
     header('Location: ../public/access_denied.php');
